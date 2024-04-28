f7e367cefe3972c1a40769d72b4e3713470d52be
sahil thakur <sahilthakurxyz@gmail.com>
2024-04-28 19:33:28 +0530

    full responsive

--- src/components/ProductsDetail/ProductDetail.js
+++ src/components/ProductsDetail/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import styles from "./productDetail.module.css";
 import ProductReviews from "./ProductReviews.js";
 import { useParams } from "react-router-dom";
@@ -33,6 +33,7 @@ import {
 import MetaData from "../labels/MetaData.js";
 import RatingStar from "../labels/RatingStar.js";
 import ScreenVisual from "../labels/ScreenVisual.js";
+import { LoginLoader } from "../../basics/Spinner.js";
 
 const ProductDetail = () => {
   const [width, setWidth] = useState(window.innerWidth); // Initial width state
@@ -139,207 +140,218 @@ const ProductDetail = () => {
   };
 
   return (
-    <div className={className} key={_id}>
-      <ScreenVisual />
-      <MetaData title={`Explore [${name}] - Details, Reviews, and More`} />
-      <div className={styles["product-details-container"]}>
-        <div className={styles["product-details"]}>
-          <div className={styles["product-details-left"]}>
-            <div className={styles["carousel-container"]}>
-              <Slider {...settings}>
-                {product.images &&
-                  product.images.map((item, i) => (
-                    <div key={item._id} className={styles["carousel-slide"]}>
-                      <img
-                        src={item.url}
-                        alt={`${i} Slide`}
-                        key={item._id}
-                        className={`${styles["carousel-image"]} ${
-                          currentSlide === i ? styles.active : ""
-                        }`}
-                      />
-                    </div>
-                  ))}
-              </Slider>
-            </div>
-          </div>
-          <div className={styles["product-details-center"]}>
-            <div className={styles["item-details-container"]}>
-              <div className={styles["items-details"]}>
-                <div className={styles["title-section-descripton"]}>
-                  <span>Descripton</span> : {description}
-                </div>
-                <div className={styles["product_rating"]}>
-                  <RatingStar {...options} rating={ratings} />
-                  {reviews && reviews.length && (
-                    <p>{`(${reviews.length} Reviews)`}</p>
-                  )}
+    <Fragment>
+      {loading ? (
+        <LoginLoader />
+      ) : (
+        <div className={className} key={_id}>
+          <ScreenVisual />
+          <MetaData title={`Explore [${name}] - Details, Reviews, and More`} />
+          <div className={styles["product-details-container"]}>
+            <div className={styles["product-details"]}>
+              <div className={styles["product-details-left"]}>
+                <div className={styles["carousel-container"]}>
+                  <Slider {...settings}>
+                    {product.images &&
+                      product.images.map((item, i) => (
+                        <div
+                          key={item._id}
+                          className={styles["carousel-slide"]}
+                        >
+                          <img
+                            src={item.url}
+                            alt={`${i} Slide`}
+                            key={item._id}
+                            className={`${styles["carousel-image"]} ${
+                              currentSlide === i ? styles.active : ""
+                            }`}
+                          />
+                        </div>
+                      ))}
+                  </Slider>
                 </div>
               </div>
-              <div className={styles["special_deals_container"]}>
-                <div className={styles["special_deal_box"]}>
-                  <div>
-                    <span>Deal of the Day</span>
-                  </div>
-                  {discount > 0 ? (
-                    <div className={styles["price-tag"]}>
-                      <FaRupeeSign /> {price}
-                      <div className={styles["cut-line"]}></div>
-                    </div>
-                  ) : (
-                    ""
-                  )}
-                  <div className={styles["product_price_off"]}>
-                    {discount > 0 ? (
-                      <p
-                        className={styles["discount"]}
-                      >{`${discount} % off `}</p>
-                    ) : (
-                      ""
-                    )}
-                    <p className={styles["product_price"]}>
+              <div className={styles["product-details-center"]}>
+                <div className={styles["item-details-container"]}>
+                  <div className={styles["cart_product_info"]}>
+                    <p className={styles["name"]}>{name}</p>
+                    <p className={styles["product_id"]}>{id}</p>
+                    <div className={styles["price"]}>
                       <FaRupeeSign />
-                      {discountedPrice ? discountedPrice.toFixed(2) : "N/A"}
-                    </p>
+                      {discountedPrice && selectedQuantity
+                        ? (discountedPrice * selectedQuantity).toFixed(2)
+                        : "N/A"}
+                      <span>
+                        <IoPricetagOutline />
+                      </span>
+                    </div>
                   </div>
+                  <div className={styles["items-details"]}>
+                    <div className={styles["title-section-descripton"]}>
+                      <span>Descripton</span> : {description}
+                    </div>
+                    <div className={styles["product_rating"]}>
+                      <RatingStar {...options} rating={ratings} />
+                      {reviews && reviews.length && (
+                        <p>{`(${reviews.length} Reviews)`}</p>
+                      )}
+                    </div>
+                  </div>
+                  <div className={styles["special_deals_container"]}>
+                    <div className={styles["special_deal_box"]}>
+                      <div>
+                        <span>Deal of the Day</span>
+                      </div>
+                      {discount > 0 ? (
+                        <div className={styles["price-tag"]}>
+                          <FaRupeeSign /> {price}
+                          <div className={styles["cut-line"]}></div>
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                      <div className={styles["product_price_off"]}>
+                        {discount > 0 ? (
+                          <p
+                            className={styles["discount"]}
+                          >{`${discount} % off `}</p>
+                        ) : (
+                          ""
+                        )}
+                        <p className={styles["product_price"]}>
+                          <FaRupeeSign />
+                          {discountedPrice ? discountedPrice.toFixed(2) : "N/A"}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles["create-review"]}>
+                    <p>
+                      Your opinion matters! Write a review and let your voice be
+                      heard.
+                    </p>
+                    <button onClick={handleReviewOpenClose}>
+                      Tell Us What You Think
+                    </button>
+                  </div>
+                  {stock > 0 && (
+                    <Dialog
+                      open={open}
+                      aria-labelledby="simple-dialog-title"
+                      onClose={handleReviewOpenClose}
+                    >
+                      <DialogTitle>Create Your Review</DialogTitle>
+                      <DialogContent className={styles["reviews-container"]}>
+                        <Rating
+                          name="simple-controlled"
+                          value={reviewRating}
+                          onChange={(e, newValue) => setReviewRating(newValue)}
+                        />
+                        <textarea
+                          id="review-textarea"
+                          className={styles["reviews-textArea"]}
+                          rows="5"
+                          cols="20"
+                          placeholder="Insert Your Review"
+                          value={reviewContent}
+                          onChange={(e) => {
+                            setReviewContent(e.target.value);
+                          }}
+                        />
+                      </DialogContent>
+                      <DialogActions>
+                        <Button
+                          color="secondary"
+                          onClick={handleReviewOpenClose}
+                        >
+                          Cancel
+                        </Button>
+                        <Button color="primary" onClick={onSubmitHandler}>
+                          Submit
+                        </Button>
+                      </DialogActions>
+                    </Dialog>
+                  )}
                 </div>
               </div>
-              <div className={styles["create-review"]}>
-                <p>
-                  Your opinion matters! Write a review and let your voice be
-                  heard.
-                </p>
-                <button onClick={handleReviewOpenClose}>
-                  Tell Us What You Think
-                </button>
+              <div className={styles["product_details_right"]}>
+                <div className={styles["user_address"]}>
+                  <div>
+                    <FaLocationDot />
+                  </div>
+                  <p className={styles["address"]}>
+                    delivering the product to you location in 5 days
+                  </p>
+                </div>
+                {stock > 0 && (
+                  <div className={styles["quantity-dropdown-container"]}>
+                    <label htmlFor="quantity" className={styles.label}>
+                      Select Quantity
+                    </label>
+                    <select
+                      id="quantity"
+                      name="quantity"
+                      value={selectedQuantity}
+                      onChange={handleQuantityChange}
+                      className={styles["dropdown"]}
+                    >
+                      {quantityOptions.map((quantity) => (
+                        <option
+                          key={quantity}
+                          value={quantity}
+                          className={styles["selected"]}
+                        >
+                          {quantity}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                )}
+                <div className={styles["stock-status"]}>
+                  <span>Status: </span>
+                  {"  "}
+                  {stock > 0 ? (
+                    <p className={styles.inStock}>In Stock</p>
+                  ) : (
+                    <p className={styles.outStock}>Out of Stock</p>
+                  )}
+                </div>
+                <hr className={styles["lines"]} />
+                <div className={styles["cart_buttons"]}>
+                  <button
+                    className={styles["add_to_cart"]}
+                    onClick={handleCartSubmit}
+                  >
+                    Add to Cart
+                  </button>
+                  <button className={styles.buy_now} onClick={handleByNow}>
+                    Buy Now
+                  </button>
+                </div>
+                <div className={styles["secure-payment"]}>
+                  <span>
+                    <GrSecure style={{ fontSize: "17px", color: "#04145e" }} />
+                  </span>
+                  <span>secure payment method</span>
+                </div>
               </div>
-              {stock > 0 && (
-                <Dialog
-                  open={open}
-                  aria-labelledby="simple-dialog-title"
-                  onClose={handleReviewOpenClose}
-                >
-                  <DialogTitle>Create Your Review</DialogTitle>
-                  <DialogContent className={styles["reviews-container"]}>
-                    <Rating
-                      name="simple-controlled"
-                      value={reviewRating}
-                      onChange={(e, newValue) => setReviewRating(newValue)}
-                    />
-                    <textarea
-                      id="review-textarea"
-                      className={styles["reviews-textArea"]}
-                      rows="5"
-                      cols="20"
-                      placeholder="Insert Your Review"
-                      value={reviewContent}
-                      onChange={(e) => {
-                        setReviewContent(e.target.value);
-                      }}
-                    />
-                  </DialogContent>
-                  <DialogActions>
-                    <Button color="secondary" onClick={handleReviewOpenClose}>
-                      Cancel
-                    </Button>
-                    <Button color="primary" onClick={onSubmitHandler}>
-                      Submit
-                    </Button>
-                  </DialogActions>
-                </Dialog>
-              )}
             </div>
           </div>
-          <div className={styles.product_details_right}>
-            <div className={styles.cart_product_info}>
-              <p className={styles.name}>{name}</p>
-              <p className={styles.product_id}>{id}</p>
-              <div className={styles.price}>
-                <FaRupeeSign />
-                {discountedPrice && selectedQuantity
-                  ? (discountedPrice * selectedQuantity).toFixed(2)
-                  : "N/A"}
-                <span>
-                  <IoPricetagOutline />
-                </span>
+          <div className={styles["review-container"]}>
+            <div className={styles["review-heading"]}>Reviews</div>
+            {reviews && reviews[0] ? (
+              <div className={styles["product-reviews"]}>
+                {reviews.map((reviewer) => (
+                  <ProductReviews key={reviewer._id} review={reviewer} />
+                ))}
               </div>
-            </div>
-            <div className={styles.user_address}>
-              <div>
-                <span>
-                  <FaLocationDot
-                    style={{ color: "#310c70", fontSize: "20px" }}
-                  />
-                </span>
-              </div>
-              <p className={styles.address}>
-                delivering the product to you location in 5 days
-              </p>
-            </div>
-            {stock > 0 && (
-              <div className={styles["quantity-dropdown-container"]}>
-                <label htmlFor="quantity" className={styles.label}>
-                  Select Quantity
-                </label>
-                <select
-                  id="quantity"
-                  name="quantity"
-                  value={selectedQuantity}
-                  onChange={handleQuantityChange}
-                  className={styles.dropdown}
-                >
-                  {quantityOptions.map((quantity) => (
-                    <option
-                      key={quantity}
-                      value={quantity}
-                      className={styles["selected"]}
-                    >
-                      {quantity}
-                    </option>
-                  ))}
-                </select>
-              </div>
+            ) : (
+              <p className={styles["empty-review"]}>No Reviews Yet</p>
             )}
-            <div className={styles["stock-status"]}>
-              <span>Status: </span>
-              {"  "}
-              {stock > 0 ? (
-                <p className={styles.inStock}>In Stock</p>
-              ) : (
-                <p className={styles.outStock}>Out of Stock</p>
-              )}
-            </div>
-            <hr className={styles.lines} />
-            <div className={styles.cart_buttons}>
-              <button className={styles.add_to_cart} onClick={handleCartSubmit}>
-                Add to Cart
-              </button>
-              <button className={styles.buy_now} onClick={handleByNow}>
-                Buy Now
-              </button>
-            </div>
-            <div className={styles["secure-payment"]}>
-              <span>
-                <GrSecure style={{ fontSize: "17px", color: "#04145e" }} />
-              </span>
-              <span>secure payment method</span>
-            </div>
           </div>
         </div>
-      </div>
-      <div className={styles["review-container"]}>
-        <div className={styles["review-heading"]}>Reviews</div>
-        {reviews && reviews[0] ? (
-          <div className={styles["product-reviews"]}>
-            {reviews.map((reviewer) => (
-              <ProductReviews key={reviewer._id} review={reviewer} />
-            ))}
-          </div>
-        ) : (
-          <p className={styles["empty-review"]}>No Reviews Yet</p>
-        )}
-      </div>
-    </div>
+      )}
+    </Fragment>
   );
 };
 
